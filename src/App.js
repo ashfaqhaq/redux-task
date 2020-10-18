c644b632d52cb9780a406e5596d39074d99f4fbe
ashfaqhaq <44408323+ashfaqhaq@users.noreply.github.com>
2020-10-18 17:23:21 +0530

    on click

--- src/App.js
+++ src/App.js
@@ -4,63 +4,69 @@ import React, { Component } from 'react'
 
 
 
- class App extends Component {
-     state={
-         body:[],
-         id:[],
-         title:[],
-         id:[]
-     }
-    componentDidMount(){
-    
+class App extends Component {
+    state = {
+        // userInput:[{
+        body: [],
+        id: [],
+        title: [],
+        // eslint-disable-next-line no-dupe-keys
+        id: []
+        // }]
+    }
+    componentDidMount() {
+
         fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data =>{
-            console.log(data);
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
 
-            // const title = data.title.map((title) => num.title);
-            const body = [];
-            // const body = [];
-            // const body = [];
-            // const body = [];
+                // const title = data.title.map((title) => num.title);
+                const body = [];
+                const title = [];
+                // const body = [];
+                // const body = [];
+                // const body = [];
 
-            for (var i = 0; i < data.length; i++) {
-                body.push(data[i])
-            }
-            // console.log(data.)
-            this.setState({
-                body
+                for (var i = 0; i < data.length; i++) {
+                    // body.push(data[i]) 
+                    body.push(data[i])
+                    //    title.push(data[i].title)
+                }
+                // console.log(data.)
+                this.setState({
+
+                    body
+
+
+
+                })
+                console.log(this.state)
             })
-            console.log(this.state)
-        })
-    
-    
-    
+
+
+
     }
 
 
     render() {
         return (
             <div>
-             {this.state.body.map((content)=>{
-                return <div className="d-flex">
-                
-                <div className="card " style={{width: '18rem'}}>
-                <div className="card-body">
-                <div className="card-title">  {content.title}  </div>
-                 <div className="card-text">
-                 {content.body}
-                 </div>
-                 </div>
-                 </div>
-                 </div>
-             })}
-            </div>
-            // <div>
-            // asda
-            // </div>
-        )
-    }
-}
+                {this.state.body.map((content,index) => {
+                    return <div>
+                    <ul>                        <li key={content.id} onClick={()=>console.log(content.id)}> {content.id}
+                        {content.title}
+                        
+                        </li>
+                        </ul>
+                    </div>
 
+                })
+            }
+            </div>
+            )
+            
+    
+}
+}
 export default App
